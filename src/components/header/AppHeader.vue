7526cc3e57a5da8bdf8c89e7c844134a73a96a11
Alex Christian <aquispe.developer@gmail.com>
2021-11-08 21:36:35 -0500

    [updated] json language

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -3,7 +3,9 @@
     <div class="d-flex flex-column">
       <div class="profile">
         <img src="assets/img/me.jpg" alt="" class="img-fluid rounded-circle" />
-        <h1 class="text-light"><a href="/portfolio">Alex Christian</a></h1>
+        <h1 class="text-light">
+          <a href="/portfolio">{{ selectedLanguage.fullName }}</a>
+        </h1>
         <div class="social-links mt-3 text-center">
           <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
           <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
@@ -14,14 +16,14 @@
       </div>
       <div class="pt-3 mx-auto">
         <div class="btn-group position-sticky text-center w-100">
-          <button type="button" class="btn btn-primary btn-sm" :class="{ 'active': language == 'en' }" @click="changeLanguage('en')">En</button>
-          <button type="button" class="btn btn-primary btn-sm" :class="{ 'active': language == 'es' }" @click="changeLanguage('es')">Es</button>
+          <button type="button" class="btn btn-primary btn-sm" :class="{ active: language == 'en' }" @click="changeLanguage('en')">En</button>
+          <button type="button" class="btn btn-primary btn-sm" :class="{ active: language == 'es' }" @click="changeLanguage('es')">Es</button>
         </div>
         <!--<button class="btn btn-sm text-white mr-5" :class="{ 'btn-primary': language == 'en' }" @click="changeLanguage('en')">-->
-          <!--<span>En</span>-->
+        <!--<span>En</span>-->
         <!--</button>-->
         <!--<button class="btn btn-sm text-white" :class="{ 'btn-primary': language == 'es' }" @click="changeLanguage('es')">-->
-          <!--<span>Es</span>-->
+        <!--<span>Es</span>-->
         <!--</button>-->
       </div>
 
@@ -29,22 +31,35 @@
       <nav id="navbar" class="nav-menu navbar">
         <ul>
           <li>
-            <a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>{{selectedLanguage.Header.menu[0]}}</span></a>
+            <a href="#hero" class="nav-link scrollto active">
+              <i class="bx bx-home"></i>
+              <span>{{ selectedLanguage.Header.menu[0] }}</span>
+            </a>
           </li>
           <li>
-            <a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>{{selectedLanguage.Header.menu[1]}}</span></a>
+            <a href="#about" class="nav-link scrollto">
+              <i class="bx bx-user"></i> <span>{{ selectedLanguage.Header.menu[1] }}</span>
+            </a>
           </li>
           <li>
-            <a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>{{selectedLanguage.Header.menu[2]}}</span></a>
+            <a href="#resume" class="nav-link scrollto"
+              ><i class="bx bx-file-blank"></i> <span>{{ selectedLanguage.Header.menu[4] }}</span>
+            </a>
           </li>
           <li>
-            <a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>{{selectedLanguage.Header.menu[3]}}</span></a>
+            <a href="#portfolio" class="nav-link scrollto">
+              <i class="bx bx-book-content"></i> <span>{{ selectedLanguage.Header.menu[5] }}</span>
+            </a>
           </li>
           <li>
-            <a href="#services" class="nav-link scrollto"><i class="bx bx-server"></i> <span>{{selectedLanguage.Header.menu[4]}}</span></a>
+            <a href="#services" class="nav-link scrollto">
+              <i class="bx bx-server"></i> <span>{{ selectedLanguage.Header.menu[6] }}</span>
+            </a>
           </li>
           <li>
-            <a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>{{selectedLanguage.Header.menu[5]}}</span></a>
+            <a href="#contact" class="nav-link scrollto">
+              <i class="bx bx-envelope"></i> <span>{{ selectedLanguage.Header.menu[8] }}</span>
+            </a>
           </li>
         </ul>
       </nav>
--- src/components/main/SectionAbout.vue
+++ src/components/main/SectionAbout.vue
@@ -2,7 +2,7 @@
   <section id="about" class="about">
     <div class="container">
       <div class="section-title">
-        <h2>About me</h2>
+        <h2>{{ selectedLanguage.Header.menu[1] }}</h2>
         <p>
           Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias
           ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
