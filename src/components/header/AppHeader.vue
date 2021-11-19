fb0931bf21f9c736457c74f0310977a14d787de7
Alex Christian <aquispe.developer@gmail.com>
2021-11-18 19:31:34 -0500

    [updated]

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -8,10 +8,10 @@
         </h1>
         <div class="social-links mt-3 text-center">
           <!--<a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>-->
-          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="https://www.facebook.com/alexchristianqr/" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <!--<a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>-->
           <!--<a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>-->
-          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+          <a href="https://www.linkedin.com/in/alex-christian-quispe-roque-3786a614a/" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
         </div>
       </div>
       <div class="pt-3 mx-auto">
