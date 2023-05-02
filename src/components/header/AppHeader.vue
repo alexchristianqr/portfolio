7d0ab3e165ba51b05f0a0dc7c841284af9cc5d63
Alex Christian <alexchristianqr@gmail.com>
2023-05-02 17:08:12 -0500

    [updated] change button languages

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -2,15 +2,19 @@
   <header id="header">
     <div class="d-flex flex-column">
       <div class="profile">
-        <img src="assets/img/me.jpg" alt="" class="img-fluid rounded-circle" />
+        <img src="assets/img/me.jpg" alt="" class="img-fluid rounded-circle"/>
         <h1 class="text-light">
           <a href="/portfolio">{{ selectedLanguage.fullName }}</a>
         </h1>
         <div class="social-links mt-3 text-center">
-          <a href="https://linkedin.com/in/alexchristianqr/" target="_blank" class="linkedin" title="LinkedIn"><i class="bx bxl-linkedin"></i></a>
-          <a href="https://github.com/alexchristianqr/" target="_blank" class="github" title="Github"><i class="bx bxl-github"></i></a>
-          <a href="https://facebook.com/alexchristianqr/" target="_blank" class="facebook" title="Facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="https://twitter.com/alexchristianqr/" target="_blank" class="twitter" title="Twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="https://linkedin.com/in/alexchristianqr/" target="_blank" class="linkedin" title="LinkedIn"><i
+              class="bx bxl-linkedin"></i></a>
+          <a href="https://github.com/alexchristianqr/" target="_blank" class="github" title="Github"><i
+              class="bx bxl-github"></i></a>
+          <a href="https://facebook.com/alexchristianqr/" target="_blank" class="facebook" title="Facebook"><i
+              class="bx bxl-facebook"></i></a>
+<!--          <a href="https://twitter.com/alexchristianqr/" target="_blank" class="twitter" title="Twitter"><i-->
+<!--              class="bx bxl-twitter"></i></a>-->
           <!--<a href="#" class="google-plus"><i class="bx bxl-google"></i></a>-->
           <!--<a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>-->
         </div>
@@ -18,12 +22,13 @@
       <div class="pt-3 mx-auto">
         <div class="btn-group" role="group" aria-label="Basic example">
           <div class="btn" style="height: 2.75rem" :class="{ active: language === 'es' }" @click="changeLanguage('es')">
-            <img :src="require('../../../public/assets/img/countries/per.svg')" alt="PERU" width="24" class="align-middle" />
-            <div v-if="language === 'es'" class="border border-bottom border-primary"></div>
+            <span v-if="language === 'es'" class="fw-bold text-white"><u>Es</u></span>
+            <span v-else class="text-white-50">Es</span>
           </div>
+          <div class="btn btn-sm fw-bold" style="height: 2.75rem; padding-left: 0; padding-right: 0">|</div>
           <div class="btn" style="height: 2.75rem" :class="{ active: language === 'en' }" @click="changeLanguage('en')">
-            <img :src="require('../../../public/assets/img/countries/usa.svg')" alt="USA" width="24" class="align-middle" />
-            <div v-if="language === 'en'" class="border border-bottom border-primary"></div>
+            <span v-if="language === 'en'" class="fw-bold text-white"><u>En</u></span>
+            <span v-else class="text-white-50">En</span>
           </div>
         </div>
       </div>
@@ -68,7 +73,8 @@
             </a>
           </li>
           <li>
-            <a href="/portfolio/AlexChristianQuispeRoque_Documentado_CV_2022.pdf" target="_blank" class="nav-link scrollto" @click="trackEvent('#curriculumvitae')">
+            <a href="/portfolio/AlexChristianQuispeRoque_Documentado_CV_2022.pdf" target="_blank"
+               class="nav-link scrollto" @click="trackEvent('#curriculumvitae')">
               <i class="bx bx-download"></i> <span>{{ selectedLanguage.Header.menu[9] }}</span>
             </a>
           </li>
